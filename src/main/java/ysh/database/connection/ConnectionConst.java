53102a469028d19f204edbfbab0c1ace42c7bc36
ysh <livefor1212@gmail.com>
2022-05-07 17:36:36 +0900

    jdbc연결2

--- src/main/java/ysh/database/connection/ConnectionConst.java
+++ src/main/java/ysh/database/connection/ConnectionConst.java
@@ -1,7 +1,7 @@
 package ysh.database.connection;
 
 public abstract class ConnectionConst {
-    private static final String URL= "jdbc:h2:tcp://localhost/~/test";
-    private static final String USERNAME = "sa";
-    private static final String PASSWORD = "";
+    public static final String URL= "jdbc:h2:tcp://localhost/~/test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
 }

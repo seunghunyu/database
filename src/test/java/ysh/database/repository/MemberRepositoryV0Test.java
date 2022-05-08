d0d2337d70a62c61e362295144e4b6c6f88e5f65
ysh <livefor1212@gmail.com>
2022-05-09 00:26:39 +0900

    SELECT 로직 추가2

--- src/test/java/ysh/database/repository/MemberRepositoryV0Test.java
+++ src/test/java/ysh/database/repository/MemberRepositoryV0Test.java
@@ -12,8 +12,13 @@ class MemberRepositoryV0Test {
     MemberRepositoryV0 repository = new MemberRepositoryV0();
     @Test
     void crud() throws SQLException {
+        //save
         Member memberV0 = new Member("memberV0", 10000);
         repository.save(memberV0);
 
+        //findById
+        Member findMember = repository.findById(memberV0.getMemberId());
+        log.info("findMember={}",findMember);
     }
+
 }

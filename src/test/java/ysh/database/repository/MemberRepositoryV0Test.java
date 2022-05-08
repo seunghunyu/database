1d23f7975e11af5cd71f7dc00f743a7f557e3228
ysh <livefor1212@gmail.com>
2022-05-09 00:33:27 +0900

    SELECT 로직 추가2

--- src/test/java/ysh/database/repository/MemberRepositoryV0Test.java
+++ src/test/java/ysh/database/repository/MemberRepositoryV0Test.java
@@ -1,11 +1,13 @@
 package ysh.database.repository;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ysh.database.domain.Member;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 @Slf4j
 class MemberRepositoryV0Test {
@@ -19,6 +21,10 @@ class MemberRepositoryV0Test {
         //findById
         Member findMember = repository.findById(memberV0.getMemberId());
         log.info("findMember={}",findMember);
+        log.info("member != findMember {}",memberV0 == findMember);
+        log.info("member equals findMember {}",memberV0.equals(findMember));
+        assertThat(findMember).isEqualTo(memberV0);
+
     }
 
 }

c9fe537911b7923232afabb95636b14bc9e5884d
ysh <livefor1212@gmail.com>
2022-05-19 00:02:40 +0900

    Datasource 예제

--- src/test/java/ysh/database/connection/ConnectionTest.java
+++ src/test/java/ysh/database/connection/ConnectionTest.java
@@ -2,7 +2,9 @@ package ysh.database.connection;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -18,5 +20,16 @@ public class ConnectionTest {
         log.info("connection = {}, class={}", con1 ,con1.getClass());
         log.info("connection = {}, class={}", con2 ,con2.getClass());
     }
-
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        //DriverManagerDataSource - 항상 새로운 커넥션을 획득
+        DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+    }
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
+        log.info("connection = {}, class={}", con1 ,con1.getClass());
+        log.info("connection = {}, class={}", con2 ,con2.getClass());
+    }
 }

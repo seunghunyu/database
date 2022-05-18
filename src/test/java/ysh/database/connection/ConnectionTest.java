1598eeda2acaa2abe4ad8c277c8984be80fc317d
ysh <livefor1212@gmail.com>
2022-05-18 23:54:22 +0900

    Datasource 예제

--- src/test/java/ysh/database/connection/ConnectionTest.java
+++ src/test/java/ysh/database/connection/ConnectionTest.java
@@ -14,6 +14,9 @@ public class ConnectionTest {
     @Test
     void driverManager() throws SQLException {
         Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection = {}, class={}", con1 ,con1.getClass());
+        log.info("connection = {}, class={}", con2 ,con2.getClass());
     }
 
 }

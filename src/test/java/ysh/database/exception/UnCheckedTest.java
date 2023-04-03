c7593538674ea0095a73664fa6d9e5609ff21d0b
ysh <livefor1212@gmail.com>
2023-04-03 23:46:25 +0900

    언체크 예외 기본

--- src/test/java/ysh/database/exception/UnCheckedTest.java
+++ src/test/java/ysh/database/exception/UnCheckedTest.java
@@ -3,60 +3,62 @@ package ysh.database.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.TestConfiguration;
 
 @Slf4j
 public class UnCheckedTest {
 
     @Test
-    void checked_Catch(){
+    void unchecked_catch(){
         Service service = new Service();
         service.callCatch();
     }
 
     @Test
-    void checked_throw(){
+    void unchecked_throw(){
         Service service = new Service();
-        Assertions.assertThatThrownBy(()-> service.callThrow())
-                .isInstanceOf(MyCheckedException.class);
+        service.callThrow();
     }
+
     /*
-    Exception을 상속받은 예외는 체크 예외가 된다.
+    RuntimeException을 상속받은 예외는 언체크 예외가 된다.
      */
-    static class MyCheckedException extends Exception{
-        public MyCheckedException(String message){
+    static class MyUnCheckedException extends RuntimeException{
+        public MyUnCheckedException(String message){
             super(message);
         }
     }
     /*
-    Checked 예외는
-    예외를 잡아서 처리하거나, 던지거나 둘중 하나를 필수로 선택해야 한다.
+    UnChecked 예외는 예외를 잡거나, 던지지 않아도 된다.
+    예외를 잡지 않으면 자동으로 밖으로 던진다.
      */
     static class Service{
         Repository repository = new Repository();
 
         /*
-        예외를 잡아서 처리하는 코드
+        필요한 경우 예외를 잡아서 처리하면 된다.
          */
         public void callCatch(){
-            try {
+            try{
                 repository.call();
-            } catch (MyCheckedException e) {
+            }catch(MyUnCheckedException e){
                 //예외 처리 로직
-                log.info("예외 처리, message={}", e.getMessage(), e);
+                log.info("예외처리, message={}", e.getMessage(), e);
             }
+
         }
         /*
-        체크 예외를 밖으로 던지는 코드
-        체크 예외는 예외를 잡지 않고 밖으로 던지려면 throws 예외를 메소드에 필수로 선언해야한다.
-        @throws MyCheckedException
+        예외를 잡지 않아도 된다.
+        체크 예외와 다르게 throws 예외 선언을 하지 않아도 된다.
          */
-        public void callThrow() throws MyCheckedException {
+        public void callThrow(){
             repository.call();
         }
+
     }
     static class Repository{
-        public void call() throws MyCheckedException {
-            throw new MyCheckedException("ex");
+        public void call(){
+            throw new MyUnCheckedException("ex");
         }
     }
 

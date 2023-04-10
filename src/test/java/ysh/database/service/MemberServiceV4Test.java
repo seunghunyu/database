31ad19363208fbe509035c272ebd233b3aa9232a
ysh <livefor1212@gmail.com>
2023-04-11 00:58:41 +0900

    JDBCTemplate 이용 반복 문제 해결

--- src/test/java/ysh/database/service/MemberServiceV4Test.java
+++ src/test/java/ysh/database/service/MemberServiceV4Test.java
@@ -11,10 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import ysh.database.domain.Member;
-import ysh.database.repository.MemberRepository;
-import ysh.database.repository.MemberRepositoryV3;
-import ysh.database.repository.MemberRepositoryV4_1;
-import ysh.database.repository.MemberRepositoryV4_2;
+import ysh.database.repository.*;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
@@ -51,7 +48,8 @@ class MemberServiceV4Test {
         @Bean
         MemberRepository memberRepository() {
 //            return new MemberRepositoryV4_1(dataSource);
-            return new MemberRepositoryV4_2(dataSource);
+//            return new MemberRepositoryV4_2(dataSource);
+            return new MemberRepositoryV5(dataSource);
         }
         @Bean
         MemberServiceV4 memberServiceV4(){
